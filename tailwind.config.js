7c1c9f329061b9901d4efad4c228f3831c351722
adethnunez <bernadettenunez005@gmail.com>
2023-05-27 15:51:12 +0800

    upp

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        dark:'#23272a',
+        light: '#ffffff',
+        primary: '#404eed',
+        grey: '#f6f6f6',
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      }
+
+    },
   },
   plugins: [],
 }
